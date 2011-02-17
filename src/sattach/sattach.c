f602a88086c9947b56863f0bebe35546562af679
Moe Jette <jette1@llnl.gov>
2011-02-17 21:51:43 +0000

    bgq geometry packing functions that work with up to 4 c-nodes. need to add support for allocations in more than one dimension

--- src/sattach/sattach.c
+++ src/sattach/sattach.c
@@ -58,6 +58,7 @@
 #include "src/common/slurm_cred.h"
 #include "src/common/slurm_protocol_api.h"
 #include "src/common/slurm_protocol_defs.h"
+#include "src/common/timers.h"
 #include "src/common/xsignal.h"
 #include "src/common/xstring.h"
 #include "src/common/xmalloc.h"
@@ -113,6 +114,175 @@ static struct io_operations message_socket_ops = {
 };
 
 static struct termios termdefaults;
+///////////////////////////////////////////////////////////////////////////////
+#define BGQ_CNODE_DIM_CNT 5
+#define BGQ_CNODE_CNT     512
+static int bgq_cnode_dim_size[BGQ_CNODE_DIM_CNT] = {4, 4, 4, 4, 2};
+
+/* Translate a 5-D coordinate into a 1-D offset in the cnode bitmap */
+static void _bgl_cnode_xlate_5d_1d(int *offset_1d, int *offset_5d)
+{
+	int i, map_offset;
+
+	xassert(offset_1d);
+	xassert(offset_5d);
+	map_offset = offset_5d[0];
+	for (i = 1; i < BGQ_CNODE_DIM_CNT; i++) {
+		map_offset *= bgq_cnode_dim_size[i];
+		map_offset += offset_5d[i];
+	}
+	*offset_1d = map_offset;
+}
+
+/* Translate a 1-D offset in the cnode bitmap to a 5-D coordinate */
+static void _bgl_cnode_xlate_1d_5d(int offset_1d, int *offset_5d)
+{
+	int i, map_offset;
+
+	xassert(offset_5d);
+
+	map_offset = offset_1d;
+	for (i = BGQ_CNODE_DIM_CNT - 1; i >= 0; i--) {
+		offset_5d[i] = map_offset % bgq_cnode_dim_size[i];
+		map_offset /= bgq_cnode_dim_size[i];
+	}
+}
+
+/* Allocate a bgq cnode map. Use bgq_cnode_map_free() to free */
+extern bitstr_t *bgq_cnode_map_alloc(void)
+{
+	bitstr_t *cnode_map = bit_alloc( BGQ_CNODE_CNT);
+	if (cnode_map == NULL)
+		fatal("bit_alloc: malloc failure");
+	return cnode_map;
+}
+
+/* Free a bgq cnode map created by bgq_cnode_map_alloc() */
+extern void bgq_cnode_map_free(bitstr_t *cnode_bitmap)
+{
+	xassert(bit_size(cnode_bitmap) == BGQ_CNODE_CNT);
+	bit_free(cnode_bitmap);
+}
+
+extern void bgq_cnode_map_set(bitstr_t *cnode_bitmap, int *offset_5d)
+{
+	int offset_1d;
+
+	_bgl_cnode_xlate_5d_1d(&offset_1d, offset_5d);
+	bit_set(cnode_bitmap, offset_1d);
+}
+
+/*
+ * Add a new allocation's cnode bitmap to that of the midplane's currently
+ *	allocated bitmap
+ */
+extern void bgq_cnode_map_add(bitstr_t *cnode_bitmap, bitstr_t *alloc_bitmap)
+{
+	xassert(bit_size(cnode_bitmap) == BGQ_CNODE_CNT);
+	xassert(bit_size(alloc_bitmap) == BGQ_CNODE_CNT);
+	bit_or(cnode_bitmap, alloc_bitmap);
+}
+
+/*
+ * Remove a terminating allocation's cnode bitmap from that of the midplane's
+ *	currently allocated bitmap
+ */
+extern void bgq_cnode_map_rm(bitstr_t *cnode_bitmap, bitstr_t *alloc_bitmap)
+{
+	xassert(bit_size(cnode_bitmap) == BGQ_CNODE_CNT);
+	xassert(bit_size(alloc_bitmap) == BGQ_CNODE_CNT);
+	bit_not(alloc_bitmap);
+	bit_and(cnode_bitmap, alloc_bitmap);
+	bit_not(alloc_bitmap);
+}
+
+/*
+ * Print the contents of a bgq cnode map created by bgq_cnode_map_alloc() or
+ *	bgq_cnode_map_test().
+ */
+extern void bgq_cnode_map_print(bitstr_t *cnode_bitmap)
+{
+	int i, j, offset[BGQ_CNODE_DIM_CNT];
+
+	xassert(cnode_bitmap);
+	xassert(bit_size(cnode_bitmap) == BGQ_CNODE_CNT);
+
+	for (i = 0; i < BGQ_CNODE_CNT; i++) {
+		if (bit_test(cnode_bitmap, i)) {
+			char dim_buf[16], full_buf[64];
+			full_buf[0] = '\0';
+			_bgl_cnode_xlate_1d_5d(i, offset);
+			for (j = 0; j < BGQ_CNODE_DIM_CNT; j++) {
+				snprintf(dim_buf, sizeof(dim_buf),  "%d ",
+				 	 offset[j]);
+				strcat(full_buf, dim_buf);
+			}
+			info("%s", full_buf);
+		}
+	}
+}
+
+/*
+ * Build bitmaps for all possible configurations of a specific c-node count
+ */
+extern int bgq_cnode_build_all(int *alloc_dims)
+{
+	int alloc_size = 1;	/* Total cnodes to be allocated */
+	int map_cnt = 0;	/* Number of bitmaps built */
+	int i, j, axis;
+	int start_offset[BGQ_CNODE_DIM_CNT], next_offset[BGQ_CNODE_DIM_CNT];
+	bitstr_t *cnode_bitmap;
+
+	xassert(alloc_dims);
+	for (j = 0; j < BGQ_CNODE_DIM_CNT; j++)
+		alloc_size *= alloc_dims[j];
+	xassert(alloc_size >= 1);
+	/* Start at location 00000 and move through all starting locations */
+	for (j = 0; j < BGQ_CNODE_DIM_CNT; j++)
+		start_offset[j] = 0;
+	for (i = 0; i < BGQ_CNODE_CNT; i++) {
+		/* Cycle through rotations in each dimension */
+		for (axis = 0; axis < BGQ_CNODE_DIM_CNT; axis++) {
+			int cnode_cnt;
+			if (bgq_cnode_dim_size[axis] < alloc_size)
+				continue;	/* too small */
+			if ((bgq_cnode_dim_size[axis] == alloc_size) &&
+			    (start_offset[axis] != 0))
+				continue;	/* already filled, no shift */
+			map_cnt++;
+			cnode_bitmap = bgq_cnode_map_alloc();
+			bgq_cnode_map_set(cnode_bitmap, start_offset);
+			for (cnode_cnt = 1; cnode_cnt < alloc_size;
+			     cnode_cnt++) {
+				for (j = 0; j < BGQ_CNODE_DIM_CNT; j++)
+					next_offset[j] = start_offset[j];
+				next_offset[axis] += cnode_cnt;
+				if (next_offset[axis] >=
+				    bgq_cnode_dim_size[axis])
+					next_offset[axis] = 0;
+				bgq_cnode_map_set(cnode_bitmap, next_offset);
+			}
+//if (map_cnt < 10) { bgq_cnode_map_print(cnode_bitmap); info(" "); }
+			if (alloc_size == 1)	/* No need to rotate */
+				break;
+		}
+		/* Move to next starting location */
+		for (j = 0; j < BGQ_CNODE_DIM_CNT; j++) {
+			if (++start_offset[j] < bgq_cnode_dim_size[j])
+				break;
+			start_offset[j] = 0;
+		}
+	}
+	return map_cnt;
+}
+///////////////////////////////////////////////////////////////////////////////
+// Time to build various maps
+// Size	Count	USec
+//    1   512     88
+//    2  2304    220
+//    3  2048    225
+//    4   512    103
+///////////////////////////////////////////////////////////////////////////////
 
 /**********************************************************************
  * sattach
@@ -125,7 +295,14 @@ int sattach(int argc, char *argv[])
 	message_thread_state_t *mts;
 	client_io_t *io;
 	char *hosts;
-
+DEF_TIMERS;
+int map_cnt;
+int alloc_dims[5] = {1, 1, 1, 1, 1};
+START_TIMER;
+map_cnt = bgq_cnode_build_all(alloc_dims);
+END_TIMER;
+info("built %d maps in %s", map_cnt, TIME_STR);
+exit(0);
 	log_init(xbasename(argv[0]), logopt, 0, NULL);
 	_set_exit_code();
 	if (initialize_and_process_args(argc, argv) < 0) {
